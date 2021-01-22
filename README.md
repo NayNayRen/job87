55fa12fd70fda8cff9bd31d50c73e5539ca555b0
Nate M <jeditendencies35@gmail.com>
2021-01-22 12:44:13 -0500

    initial commit of Job 87

---
+++ README.md
@@ -0,0 +1,3 @@
+This project, Job 87, is a rebuild of an ePortfolio/Blog and eLearning website of Jason C. All content used is of ownership by Jason
+and used with his permission. None of the social media or email join list is not connected. This is just a project built for myself to be used
+on my portfolio site as something I've built. Thank you.
---
+++ scripts.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.nav-box');
+const scrollPoint = document.querySelector('#scroll-point');
+
+/*function fixedNav() {
+  if (document.documentElement.scrollTop > 0) {// scroll point in pixels to start the fixed position
+    nav.classList.add('fixed');
+    scrollPoint.style.paddingTop = '75px';
+  } else {
+    nav.classList.remove('fixed');
+    scrollPoint.style.paddingTop = '75px';
+  }
+
+}
+
+window.addEventListener('scroll', fixedNav);*/
